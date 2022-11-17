77d8a98c93afb73915f50f4095cc64ba1d4f848f
KricsDavid <71211940+KricsDavid@users.noreply.github.com>
2022-11-17 13:53:55 +0100

    Add files via upload

---
+++ Program tenger kérdések.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleApp83
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            const char TENGER_JEL = '*';
+            const char SZIGET_JEL = 'O';
+            const char HAJO_JEL = '█';
+            /*
+            char[,] tenger = new char[SOROK_SZAMA, OSZLOPOK_SZAMA];
+            */
+            //todo Itt majd be kell kérni a játékostól a pálya méretét - Ezt Zoli intézi majd
+            const int SOROK_SZAMA = 20;
+            const int OSZLOPOK_SZAMA = 60;
+            char[,] tenger = new char[SOROK_SZAMA, OSZLOPOK_SZAMA];
+
+            for (int sorIndex = 0; sorIndex < tenger.GetLength(0); sorIndex++)
+            {
+                for (int oszlopIndex = 0; oszlopIndex < tenger.GetLength(1); oszlopIndex++)
+                {
+                    tenger[sorIndex, oszlopIndex] = TENGER_JEL;
+                }
+            }
+            Random vel = new Random();
+            for (int i = 0; i < 50; i++)
+            {
+                tenger[vel.Next(tenger.GetLength(0)), vel.Next(tenger.GetLength(1))] = SZIGET_JEL;
+            }
+            Megjelenit(tenger);
+
+            //1) Hány sziget van a tengeren?
+            //2) Hány sziget van a tenger szélén?
+            //3) Van-e olyan sziget, ami mellett közvetlenül másik sziget is van?
+        }
+
+        static void Megjelenit(char[,] terkep)
+        {
+            Console.Clear();
+            for (int sorIndex = 0; sorIndex < terkep.GetLength(0); sorIndex++)
+            {
+                for (int oszlopIndex = 0; oszlopIndex < terkep.GetLength(1); oszlopIndex++)
+                {
+                    Console.Write(terkep[sorIndex, oszlopIndex]);
+                }
+                Console.WriteLine();
+         
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ RandomJelZenere.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp84
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int szamlalo = 10000;
+            int kuszob = (9000 - 500) / 26;
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine(szamlalo);
+                System.Threading.Thread.Sleep(100);
+                szamlalo--;
+                if (Console.KeyAvailable)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    char karakter = Console.ReadKey().KeyChar;
+                    Console.SetCursorPosition(rnd.Next(Console.WindowWidth), rnd.Next(1, Console.WindowHeight));
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.Write(karakter);
+                    //'a' - 500  'z' - 9000
+                    
+
+                    Console.Beep((Convert.ToInt32(karakter)-96) * kuszob, 100);
+                }
+
+
+                //Console.WriteLine(new string('─', 60));
+                //Console.WriteLine(bill.KeyChar);
+                //Console.WriteLine(bill.Key.ToString());
+                //Console.WriteLine(bill.Modifiers.ToString());
+            }
+
+        }
+    }
+}
